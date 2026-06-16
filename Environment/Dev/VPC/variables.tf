4acfc38b4e1071b1bd16508234f3022332b886fd
Raghu <raghu@gmail.com>
2026-06-17 00:21:45 +0530

    Added VPC Module

--- Environment/Dev/VPC/variables.tf
+++ Environment/Dev/VPC/variables.tf
@@ -138,9 +138,6 @@ variable "project_name" {
   type = string
 }
 
-variable "environment" {
-  type = string
-}
 
 variable "sg_name" {
   type = string
@@ -154,9 +151,6 @@ variable "vpc_id" {
   type = string
 }
 
-variable "common_tags" {
-  type = map
-}
 
 variable "sg_tags" {
   type = map
