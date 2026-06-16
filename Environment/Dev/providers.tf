3ead08946c4aca7adac3901b0e145babd1ff9f2c
omprakash-253 <omprakashdevops253@gmail.com>
2026-06-16 22:18:33 +0530

    buck name Updated

--- Environment/Dev/providers.tf
+++ Environment/Dev/providers.tf
@@ -6,7 +6,7 @@ terraform {
         }
     }
     backend "s3" {
-        bucket = "omansh-remote-state"
+        bucket = "omansh-remote-state1"
         key    = "prod/ec2/terraform.tfstate"
         region = "us-east-1"
         encrypt = true
